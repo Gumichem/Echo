aa115a4f5f7265ad7add844efe00be46ba70e99e
Gumichem <gumichem@gmail.com>
2015-11-29 21:44:34 +0200

    Добавил класс Echo, переименовал главный класс в EchoTestDrive

---
+++ src/Puzzle/Echo.java
@@ -0,0 +1,12 @@
+package Puzzle;
+
+/**
+ * In project Echo:
+ * Packaged by D.Gumeniuk in Puzzle on 29.11.2015
+ */
+public class Echo {
+    int count=0;
+    void hello(){
+        System.out.println("Привееееет...");
+    }
+}
--- src/Puzzle/EchoTestDrive.java
+++ src/Puzzle/EchoTestDrive.java
@@ -1,8 +1,8 @@
 package Puzzle;
 
-public class Main {
+public class EchoTestDrive {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
